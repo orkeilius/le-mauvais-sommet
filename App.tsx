ae3c95e0aa86f4a3d671f963fd1f23869dfa9df3
Matthieu D. <orkeilius@gmail.com>
2025-04-25 12:00:42 +0200

    add exemple in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import AppNavigator from "@/src/app/navigation/app-navigator"
+import UserRepository from "@/src/Repository/UserRepository";
 
 export default function App() {
+    UserRepository.getInstance().getById(102).then(
+        user => {
+            console.log(user)
+            user.getAuction().then(auction => {console.log(auction)})
+        }
+    )
+
   return (
     <SafeAreaProvider>
       <AppNavigator />
